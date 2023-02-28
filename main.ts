8e7b2f0d1379738daf1045a2801b6384fb001a8e
CoderDojoAizu <42738247+CoderDojoAizu@users.noreply.github.com>
2023-03-01 08:32:31 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,21 @@
+let 計測開始時間 = 0
+let 測定時間 = 0
 input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    計測開始時間 = input.runningTime()
+})
+input.onButtonPressed(Button.B, function () {
     if (計測開始時間 > 0) {
         測定時間 = (input.runningTime() - 計測開始時間) / 1000
         basic.showNumber(測定時間)
-    }
-    if (計測開始時間 == 0) {
-        basic.clearScreen()
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . # # # .
-            . . # . .
-            . . . . .
-            `)
-        計測開始時間 = input.runningTime()
+        計測開始時間 = 0
+    } else {
+        basic.showNumber(測定時間)
     }
 })
-input.onButtonPressed(Button.AB, function () {
-    basic.showIcon(IconNames.Diamond)
-    計測開始時間 = 0
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(測定時間)
-})
-let 測定時間 = 0
-let 計測開始時間 = 0
-basic.showIcon(IconNames.Diamond)
-計測開始時間 = 0
